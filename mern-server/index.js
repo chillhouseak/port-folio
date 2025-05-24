e7ee2a8836b7be0e2abb7661ad552089a6fa4808
chillhouseak <139872607+chillhouseak@users.noreply.github.com>
2025-05-24 16:14:17 +0530

    Update index.js

--- mern-server/index.js
+++ mern-server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.PORT||3000;
 app.use(cors());
 app.use(express.json());
+
+const port = process.env.PORT||3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
